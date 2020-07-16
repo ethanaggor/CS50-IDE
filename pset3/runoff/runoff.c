dc45bfac238c52cc6bf3a7c54daaa7bca28f4c16
Ethan Aggor <ethan.aggor@gmail.com>
2020-07-16 22:44:14 +0000

    Add is_tie

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -195,7 +195,22 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    int j = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes == min)
+            {
+               j++;
+            }
+        }
+    }
+    if (j != 0)
+    {
+        return true;
+    }
     return false;
 }
 
