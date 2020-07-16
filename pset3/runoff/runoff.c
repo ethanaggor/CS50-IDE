93a33c047dd81ef262b4ef34ccb4281fc2f46d15
Ethan Aggor <ethan.aggor@gmail.com>
2020-07-16 23:12:45 +0000

    Update vote

--- pset3/runoff/runoff.c
+++ pset3/runoff/runoff.c
@@ -134,7 +134,8 @@ for (int j = 0; j < candidate_count; j++)
    {
        if (strcmp(name, candidates[j].name) == 0)
        {
-           preferences[i][j] = j;
+           preferences[voter][rank] = j;
+
            return true;
        }
    }
